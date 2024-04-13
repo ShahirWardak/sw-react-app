887b225a5f07d3e1b1b1f6ef35f9247b81c30aa5
ShahirWardak <shahir.wardak@rspb.org.uk>
2024-04-13 22:53:24 +0100

    Update overall colors and styles

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='border-t border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-900'>
+    <footer className='dark:bg-discord-900 border-t border-gray-200 bg-white dark:border-stone-600'>
       <div>Example footer content</div>
     </footer>
   );
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,7 +1,9 @@
-export default function Navbar() {
+import { pageRoutes } from '../types/routes.types';
+
+export default function Navbar(props: { routes: pageRoutes[] }) {
   return (
-    <nav className='fixed start-0 top-0 z-20 w-full border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-900'>
-      <div className='mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4'>
+    <nav className='dark:bg-discord-900 fixed start-0 top-0 z-20 w-full border-b border-gray-200 bg-white dark:border-stone-600'>
+      <div className='mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-3'>
         <a href='/' className='flex items-center space-x-3 rtl:space-x-reverse'>
           <span className='self-center whitespace-nowrap text-4xl font-semibold dark:text-white'>
             React App
@@ -10,11 +12,9 @@ export default function Navbar() {
         <div className='flex items-center space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse'>
           <a
             href='#'
-            className='group relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-green-400 to-blue-600 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-200 group-hover:from-green-400 group-hover:to-blue-600 dark:text-white dark:focus:ring-green-800'
+            className='hover:bg-discord-300 rounded border border-gray-200 bg-transparent px-4 py-2 font-semibold hover:text-white dark:text-white dark:hover:text-white'
           >
-            <span className='relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900'>
-              Account
-            </span>
+            Account
           </a>
           <button
             data-collapse-toggle='navbar-sticky'
@@ -33,9 +33,9 @@ export default function Navbar() {
             >
               <path
                 stroke='currentColor'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
                 d='M1 1h15M1 7h15M1 13h15'
               />
             </svg>
@@ -45,40 +45,18 @@ export default function Navbar() {
           className='hidden w-full items-center justify-between md:order-1 md:flex md:w-auto'
           id='navbar-sticky'
         >
-          <ul className='mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900'>
-            <li>
-              <a
-                href='/dashboard'
-                className='block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500'
-                aria-current='page'
-              >
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a
-                href='/messenger'
-                className='block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500'
-              >
-                Messenger
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500'
-              >
-                Link 3
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500'
-              >
-                Link 4
-              </a>
-            </li>
+          <ul className='mt-4 flex flex-col rounded-lg border border-gray-100 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 rtl:space-x-reverse'>
+            {props.routes.map((page, i) => (
+              <li key={i} className='md:dark:hover:bg-discord-300 rounded p-3'>
+                <a
+                  href={page.url}
+                  className='block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-stone-300 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-stone-100'
+                  aria-current='page'
+                >
+                  {page.title}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
