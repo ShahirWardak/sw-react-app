975c090fb7de0cb62ea023a6a81dafe799fda9f2
ShahirWardak <shahir.wardak@rspb.org.uk>
2024-04-13 15:23:52 +0100

    Cleared default homepage content

---
+++ src/app/modules/pages/homepage.tsx
@@ -0,0 +1,7 @@
+export default function Homepage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>Homepage works!</p>
+    </main>
+  );
+}
