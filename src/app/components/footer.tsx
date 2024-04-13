da624164d93a6dc2365d5b69ebf85df27ae09682
ShahirWardak <shahir.wardak@rspb.org.uk>
2024-04-13 19:06:00 +0100

    Navbar styling update

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className='border-t border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-900'>
       <div>Example footer content</div>
     </footer>
   );
