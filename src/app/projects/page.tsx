3f6d982c5088c661ce334c350d5b1d1de1fc3d03
ShahirWardak <shahir.wardak@rspb.org.uk>
2024-04-17 17:48:58 +0100

    Project page content and new link icon component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,5 @@
 import Container from '../components/container';
+import IconList from '../components/icon-list';
 import LogoText from '../components/logo-text';
 
 export default function Dashboard() {
@@ -7,7 +8,7 @@ export default function Dashboard() {
       <h1 className='text-5xl font-semibold'>PROJECTS</h1>
       <div className='relative flex place-items-center'>
         <Container>
-          <h1 className='text-4xl font-semibold'>RSPB</h1>
+          <h1 className='text-4xl font-semibold md:mb-5'>RSPB</h1>
           <LogoText
             logos={[
               {
@@ -18,13 +19,39 @@ export default function Dashboard() {
               },
             ]}
           >
-            <ul className='mt-5 list-inside list-disc'>
-              <li>Test longer text</li>
-              <li>test</li>
-              <li>test</li>
-              <li>test</li>
-              <li>test</li>
-            </ul>
+            <div className='mt-5 flex-col md:mt-0'>
+              <a
+                href='https://www.rspb.org.uk'
+                className='font-medium text-blue-600 underline underline-offset-2 dark:text-blue-500'
+                target='_blank'
+              >
+                www.rspb.org.uk
+              </a>
+              <ul className='mt-5 list-inside list-disc text-left'>
+                <li>Angular framework</li>
+                <li>HTML, Sass, TypeScript</li>
+                <li>C#</li>
+                <li>Mangolia CMS</li>
+                <li>Microsoft Azure</li>
+              </ul>
+              <IconList
+                icons={[
+                  {
+                    src: '/svg/react.svg',
+                    alt: 'RSPB Logo',
+                  },
+                  {
+                    src: '/svg/next.svg',
+                    alt: 'RSPB Logo',
+                    invert: true,
+                  },
+                  {
+                    src: '/svg/tailwindcss.svg',
+                    alt: 'RSPB Logo',
+                  },
+                ]}
+              ></IconList>
+            </div>
           </LogoText>
         </Container>
       </div>
--- src/app/types/logo-text.types.ts
+++ src/app/types/logo-text.types.ts
@@ -1,4 +1,4 @@
-export type imageLogo = {
+type imageLogo = {
     src: string;
     alt: string;
     width: number;
