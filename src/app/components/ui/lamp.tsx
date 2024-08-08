fcfe3d322e32a7e902a97ad9c162bc28916f6ac1
ShahirWardak <shahir.wardak@rspb.org.uk>
2024-08-08 13:56:54 +0100

    lamp styles

--- src/app/components/ui/lamp.tsx
+++ src/app/components/ui/lamp.tsx
@@ -32,7 +32,7 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        'relative z-0 flex min-h-screen w-full flex-col items-center justify-center overflow-hidden rounded-md bg-slate-950',
+        'relative z-0 flex min-h-screen w-full flex-col items-center justify-center overflow-hidden rounded-md bg-discord-900',
         className
       )}
     >
@@ -50,8 +50,8 @@ export const LampContainer = ({
           }}
           className='bg-gradient-conic absolute inset-auto right-1/2 h-56 w-[30rem] overflow-visible from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]'
         >
-          <div className='absolute  bottom-0 left-0 z-20 h-40 w-[100%] bg-slate-950 [mask-image:linear-gradient(to_top,white,transparent)]' />
-          <div className='absolute  bottom-0 left-0 z-20 h-[100%]  w-40 bg-slate-950 [mask-image:linear-gradient(to_right,white,transparent)]' />
+          <div className='absolute  bottom-0 left-0 z-20 h-40 w-[100%] bg-discord-900 [mask-image:linear-gradient(to_top,white,transparent)]' />
+          <div className='absolute  bottom-0 left-0 z-20 h-[100%]  w-40 bg-discord-900 [mask-image:linear-gradient(to_right,white,transparent)]' />
         </motion.div>
         <motion.div
           initial={{ opacity: 0.5, width: '15rem' }}
@@ -66,10 +66,10 @@ export const LampContainer = ({
           }}
           className='bg-gradient-conic absolute inset-auto left-1/2 h-56 w-[30rem] from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]'
         >
-          <div className='absolute  bottom-0 right-0 z-20 h-[100%]  w-40 bg-slate-950 [mask-image:linear-gradient(to_left,white,transparent)]' />
-          <div className='absolute  bottom-0 right-0 z-20 h-40 w-[100%] bg-slate-950 [mask-image:linear-gradient(to_top,white,transparent)]' />
+          <div className='absolute  bottom-0 right-0 z-20 h-[100%]  w-40 bg-discord-900 [mask-image:linear-gradient(to_left,white,transparent)]' />
+          <div className='absolute  bottom-0 right-0 z-20 h-40 w-[100%] bg-discord-900 [mask-image:linear-gradient(to_top,white,transparent)]' />
         </motion.div>
-        <div className='absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl'></div>
+        <div className='absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-discord-900 blur-2xl'></div>
         <div className='absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md'></div>
         <div className='absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl'></div>
         <motion.div
@@ -93,7 +93,7 @@ export const LampContainer = ({
           className='absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400 '
         ></motion.div>
 
-        <div className='absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 '></div>
+        <div className='absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-discord-900 '></div>
       </div>
 
       <div className='relative z-50 flex -translate-y-80 flex-col items-center px-5'>
