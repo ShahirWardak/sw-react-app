fba6ff84f8aa5d00198f68937745eb7efde00e36
ShahirWardak <shahir.wardak@rspb.org.uk>
2025-11-06 22:13:21 +0000

    WIP sticky nav functions

--- src/app/components/sticky-nav.tsx
+++ src/app/components/sticky-nav.tsx
@@ -1,10 +1,25 @@
-export default function StickyNav() {
+type Section = {
+  id: string;
+  label: string;
+  ref: React.RefObject<HTMLElement>;
+};
+
+interface StickyNavProps {
+  sections: Section[];
+}
+
+export default function StickyNav({ sections }: StickyNavProps) {
+  function handleClick(ref: React.RefObject<HTMLElement>) {
+    console.log(ref);
+  }
+
   return (
-    <div className='z-50 flex justify-end gap-5 bg-neutral-800 p-5 px-20 text-lg font-bold shadow-[0px_8px_10px_2px_#000000]'>
-      <a className='text-cyan-400'>Nav Item</a>
-      <a>Nav Item</a>
-      <a>Nav Item</a>
-      <a>Nav Item</a>
+    <div className='sticky top-0 z-50 flex justify-end gap-5 bg-neutral-800 p-5 px-20 text-lg font-bold shadow-[0px_8px_10px_2px_#000000]'>
+      {sections.map((item) => (
+        <a key={item.id} onClick={() => handleClick(item.ref)}>
+          {item.label}
+        </a>
+      ))}
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useRef } from 'react';
 import IntroLamp from './components/sections/intro-lamp';
 import IntroText from './components/sections/intro-text';
 import { Timeline } from './components/ui/timeline';
@@ -10,8 +10,21 @@ import { StarsBackground } from './components/ui/stars-background';
 import StickyNav from './components/sticky-nav';
 
 export default function Home() {
+  const homeRef = useRef<HTMLDivElement | null>(null);
+  const aboutRef = useRef<HTMLDivElement | null>(null);
+  const worksRef = useRef<HTMLDivElement | null>(null);
+  const contactRef = useRef<HTMLDivElement | null>(null);
+
+  const sections = [
+    { id: 'home', label: 'Home', ref: homeRef },
+    { id: 'about', label: 'About Me', ref: aboutRef },
+    { id: 'works', label: 'My Works', ref: worksRef },
+    { id: 'contact', label: 'Contact', ref: contactRef },
+  ];
+
   return (
     <>
+      <span ref={homeRef}></span>
       <IntroLamp heading='Shahir Wardak' />
 
       <div className='mb-16 lg:mb-32'></div>
@@ -20,12 +33,13 @@ export default function Home() {
 
       <div className='mb-10 lg:mb-20'></div>
 
-      <StickyNav />
+      <StickyNav sections={sections} />
 
       <div className='mb-10 lg:mb-20'></div>
 
       <div className='container relative'>
         <h2
+          ref={aboutRef}
           id='about-section'
           className='mb-10 text-center text-2xl font-extrabold text-black dark:text-neutral-300 md:text-4xl'
         >
@@ -37,6 +51,7 @@ export default function Home() {
 
         <div className='relative w-full overflow-clip'>
           <h2
+            ref={worksRef}
             id='timeline-section'
             className='text-center text-2xl font-extrabold text-black dark:text-neutral-300 md:text-4xl'
           >
@@ -48,6 +63,7 @@ export default function Home() {
         <div className='mb-32 lg:mb-60'></div>
 
         <h2
+          ref={contactRef}
           id='contact-section'
           className='mb-10 text-center text-2xl font-extrabold text-black dark:text-neutral-300 md:text-4xl'
         >
